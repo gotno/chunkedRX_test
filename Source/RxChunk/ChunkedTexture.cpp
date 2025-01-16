d18007c0fb2300716debe360edba26bbd154cab0
Will Floyd <floyd.will@gmail.com>
2025-01-15 21:11:32 -0800

    working image decompression

--- Source/RxChunk/ChunkedTexture.cpp
+++ Source/RxChunk/ChunkedTexture.cpp
@@ -1,5 +1,8 @@
 #include "ChunkedTexture.h"
 
+#define QOI_IMPLEMENTATION
+#include "ThirdParty/qoi/qoi.h"
+
 #include "Engine/Texture2D.h"
 
 void UChunkedTexture::SetDimensions( int32_t inWidth, int32_t inHeight) {
@@ -8,6 +11,10 @@ void UChunkedTexture::SetDimensions( int32_t inWidth, int32_t inHeight) {
 }
 
 UTexture2D* UChunkedTexture::MakeTexture() {
+  qoi_desc filedesc;
+  uint8* uncompressed =
+    (uint8*)qoi_decode(Data.GetData(), Data.Num(), &filedesc, 4);
+
   UTexture2D* TextureTarget =
     UTexture2D::CreateTransient(Width, Height, PF_R8G8B8A8);
 
@@ -16,11 +23,12 @@ UTexture2D* UChunkedTexture::MakeTexture() {
       GetPlatformData()->
       Mips[0].BulkData.Lock(LOCK_READ_WRITE);
 
-  FMemory::Memcpy(MipData, Data.GetData(), Width * Height * 4);
+  FMemory::Memcpy(MipData, uncompressed, Width * Height * 4);
 
   TextureTarget->GetPlatformData()->Mips[0].BulkData.Unlock();
   TextureTarget->UpdateResource();
 
+  free(uncompressed);
   return TextureTarget;
 }
 
